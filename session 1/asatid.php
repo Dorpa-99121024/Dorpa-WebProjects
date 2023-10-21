673c02ade190ce373f845169ed6285e464c4817f
Dorpa-99121024 <amir.dor86pa@gmail.com>
2023-10-22 00:39:06 +0330

    third commit

---
+++ session 1/asatid.php
@@ -0,0 +1,13 @@
+<?php 
+
+$asatid = [
+    'ostad1' => [
+        'name' => 'DR. Akhoundi',
+    ],
+    'ostad2' => [
+        'name' => 'DR. Mottaghi'
+    ],
+    'ostad3' => [
+        'name' => 'DR. Mortazavi'
+    ],
+];
--- session 1/index.php
+++ session 1/index.php
@@ -1,22 +1,31 @@
+<?php
+require_once('./functions.php'); 
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>تمرین جلسه دوم برنامه نویسی وب</title>
+        <title>تمرین درس برنامه نویسی وب</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     </head>
+    <?php
+    require('./lessons.php');
+    require('./asatid.php');
+    dd($asatid);
+     
+    ?>
     <body dir="rtl">
-        <h1>برنامه ترم اول سال تحصیلی 1402 - 1401</h1>
+        <h1>برنامه ترم اول سال تحصیلی 1402 - 1401 </h1>
         <div class="row col-md-6 col-sm-12" style="margin-right:auto;margin-left:auto;">
             <table class="table table-bordered table-striped">
-                <tr>
-                    <th># #</th>
-                    <th>10 - 8</th>
-                    <th>12 - 10</th>
-                    <th>15:30 - 13:30</th>
-                    <th>17:30 - 15:30</th>
-                </tr>
+                <thead>
+                    <th>#</th>
+                    <th>8-10</th>
+                    <th>10-12</th>
+                    <th>13:30-15:30</th>
+                    <th>15:30-17:30</th>
+                </thead>
                 <?php
                 $week_days = [
                     'شنبه',
@@ -26,6 +35,7 @@
                     'چهارشنبه',
                 ] 
                 ?>
+                <tbody>
                 <?php for($cnt=0;$cnt<5;$cnt++){ ?>
                     <tr>
                         <td><?php echo($week_days[$cnt]) ?></td>
@@ -35,6 +45,7 @@
                         <td>#</td>
                     </tr>
                 <?php } ?>
+                </tbody>
             </table>
         </div>
     </body>
